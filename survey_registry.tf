67ff611176290e55e916082d387eb8a0862c320a
SamGodwin2 <sam.godwin@ext.ons.gov.uk>
2019-08-13 08:04:20 +0100

    more tests

--- survey_registry.tf
+++ survey_registry.tf
@@ -1,11 +1,11 @@
 resource "aws_dynamodb_table" "survey_registry_table" {
   name           = "${var.env}-survey-registry"
-  hash_key       = "id"
+  hash_key       = "eq_id"
   range_key      = "updatedAt"
   billing_mode   = "PAY_PER_REQUEST"
 
   attribute {
-    name = "id"
+    name = "eq_id"
     type = "S"
   }
 
